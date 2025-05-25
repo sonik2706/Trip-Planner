1ca672c248a8de2fdf08b2a66b654f399ddacbe5
sonek2706 <s26092@pjwstk.edu.pl>
2025-05-25 13:46:54 +0200

    Init graph

---
+++ app/graph.py
@@ -0,0 +1,120 @@
+"""
+
+"""
+import graphviz
+
+from langgraph.graph import StateGraph, END
+from typing import TypedDict
+
+from agents.prompt_agent import PromptAgent
+from agents.attraction_agent import AttractionAgent
+from agents.hotel_agent import HotelAgent
+from agents.map_agent import MapAgent
+
+from settings.config import Config
+
+class State(TypedDict, total=False):
+    country: str
+    city: str
+    user_input: str
+    trip_preferences: dict
+    attractions: list
+    hotels: list
+    itinerary: dict
+    hotel_params: dict
+
+
+class Graph:
+    def __init__(self):
+        self.config = Config()
+        self.prompt_agent = PromptAgent(self.config)
+        self.hotel_agent = HotelAgent(self.config)
+        self.attraction_agent = AttractionAgent(self.config)
+        self.map_agent = MapAgent(self.config)
+        self.graph = self._build_graph()
+
+    def _build_graph(self):
+        graph = StateGraph(State)
+
+        # Define nodes
+        graph.add_node("verify_prompt", self._verify_prompt)
+        graph.add_node("search_for_attractions", self._generate_attractions)
+        graph.add_node("find_hotels", self._generate_hotels)
+        graph.add_node("plan_the_trip", self._build_itinerary)
+        graph.add_node("generate_response", self._generate_response)
+
+        # Set entry and conditional routing
+        graph.set_entry_point("verify_prompt")
+        
+        graph.add_edge("verify_prompt", "search_for_attractions")
+        graph.add_edge("verify_prompt", "find_hotels")
+
+        # Final steps
+        graph.add_edge("search_for_attractions", "plan_the_trip")
+        graph.add_edge("find_hotels", "plan_the_trip")
+        graph.add_edge("plan_the_trip", "generate_response")
+        graph.add_edge("generate_response", END)
+
+        self._raw_graph = graph
+
+        return graph.compile()
+
+    def run(self, country: str, city: str, user_input: str, hotel_params: dict) -> dict:
+        initial_state = {
+            "country": country,
+            "city": city,
+            "user_input": user_input,
+            "hotel_params": hotel_params
+        }
+        return self.graph.invoke(initial_state)
+
+    # === Node Functions ===
+    def _verify_prompt(self, state: State) -> State:
+        if state.get("user_input", "").strip():
+            print("SKIPPING PROMT VERIFICATION!!!")
+            return state
+
+        preferences = self.prompt_agent.extract(state["country"], state["city"],state["user_input"])
+        state["trip_preferences"] = preferences
+        return state
+
+    def _generate_attractions(self, state: State) -> State:
+        prefs = state["trip_preferences"] # TO BE ADDED!!!
+        city = prefs.get("city", "Rome")
+        focus = prefs.get("focus", None)
+        attractions = self.attraction_agent.find_attractions(
+            city_name=city, num_attractions=10, focus=focus
+        )
+        state["attractions"] = attractions
+        return state
+
+    def _generate_hotels(self, state: State) -> State:
+        hotel_params = state["hotel_params"]
+        hotels = self.hotel_agent.fetch_hotels_from_booking(params=hotel_params)
+        state["hotels"] = hotels
+        return state
+
+    def _build_itinerary(self, state: State) -> State:
+        city = state["trip_preferences"].get("city", "Rome")
+        accomodation = (
+            state["hotels"][0]["location"] if state["hotels"] else "city center"
+        )
+        days = 3
+        attractions = [a["name"] for a in state["attractions"]]
+        itinerary = self.map_agent.optimize(
+            city=city,
+            days=days,
+            accomodation_address=accomodation,
+            list_attractions=attractions,
+        )
+        state["itinerary"] = itinerary
+        return state
+
+    def _generate_response(self):
+        pass
+
+    def visualize_graph(self, filename="manager_graph.jpg"):
+
+        img_bytes = self.graph.get_graph().draw_mermaid_png()
+        with open("graph.png", "wb") as f:
+            f.write(img_bytes)
--- main.py
+++ main.py
@@ -6,10 +6,8 @@ import openai
 import os
 import time
 
-from agents.attraction_agent import AttractionAgent
-from agents.hotel_agent import HotelAgent
-from agents.map_agent import MapAgent
-from agents.prompt_agent import PromptAgent
+from app.graph import Graph
+
 
 DEFAULT_PARAMS = {
     "country": "Poland",
@@ -55,7 +53,7 @@ with st.sidebar:
     )
     max_hotels = st.number_input("Maximum Number of Hotels", min_value=1, value=DEFAULT_PARAMS["max_hotels"])
 
-params = {
+hotel_params = {
     "country": country,
     "city": city,
     "checkin_date": checkin_date.isoformat(),
@@ -71,96 +69,20 @@ params = {
     "max_hotels": max_hotels,
 }
 
-
 # --- Streamlit UI ---
 st.title("Trip Planner")
 st.markdown("---")
 
-user_description = st.text_area("Write your travel preferences or comments here")
+user_description = st.text_area("Write your travel preferences or comments here...")
 
-hotel_agent = HotelAgent()
-
-prompt_agent = PromptAgent()
+app = Graph()
 
 # Initialize session_state for hotels if not set
-if "hotels_data" not in st.session_state:
-    st.session_state.hotels_data = []
-
-if "attractions" not in st.session_state:
-    st.session_state.attractions = []
+if "trip_plan" not in st.session_state:
+    st.session_state.trip_plan = []
 
 if st.button("Run"):
-    response = prompt_agent.extract(country ,city , user_description)
-    st.write(response)
-
-col1, col2 = st.columns([1, 1])
-
-with col1:
-    if st.button("🔍 Search Hotels"):
-        if checkin_date >= checkout_date:
-            st.error("Check-out date must be later than check-in date.")
-            st.stop()
-
-        with st.spinner("Fetching hotels..."):
-            hotels = []
-            try:
-                hotels = hotel_agent.fetch_hotels_from_booking(params)
-                st.session_state.hotels_data = hotels
-            except Exception as e:
-                st.error(e)
-
-with col2:
-    generate_disabled = not st.session_state.get("hotels_data")
-    st.button("🧭 Generate Plan", disabled=generate_disabled)
-
-    # with st.spinner("✅ Validating attraction locations using Nominatim..."):
-    #     valid_attractions = hotel_agent.get_valid_locations_with_coords(st.session_state.attractions)
-
-    # if not valid_attractions:
-    #     st.warning("No valid attractions found.")
-    # if not hotels:
-    #     st.warning("No valid hotels found.")
-
-    # enriched_hotels = hotel_agent.enrich_hotels_with_distances(hotels, valid_attractions)
-    # enriched_hotels = [h for h in enriched_hotels if h.get("avg_distance") is not None]
-
-    # # Sorting
-    # if params["sort_by"] == "price":
-    #     enriched_hotels = sorted(enriched_hotels, key=lambda x: x.get("price", float('inf')))
-    # elif params["sort_by"] == "review_score":
-    #     enriched_hotels = sorted(enriched_hotels, key=lambda x: x.get("review_score", 0), reverse=True)
-    # elif params["sort_by"] == "shortest_distance":
-    #     enriched_hotels = sorted(enriched_hotels, key=lambda x: x.get("avg_distance", float('inf')))
-
-    # # Limit number of results
-    # enriched_hotels = enriched_hotels[:params.get("max_hotels", 10)]
-
-    # # Save to session_state
-    # st.session_state.hotels_data = enriched_hotels
-    # st.session_state.valid_attractions = valid_attractions
-
-# Display hotel results if available
-if st.session_state.hotels_data:
-    st.subheader(f"Found Hotels in {params['city']}")
-    for hotel in st.session_state.hotels_data:
-        st.markdown("----")
-        st.markdown(f"### 🏨 [{hotel['name']}]({hotel.get('link', '#')})")
-
-        col1, col2 = st.columns(2)
-        with col1:
-            st.markdown(f"💰 **Price:** {hotel['price']} {params['currency']}")
-            st.markdown(f"📍 **Address:** {hotel['location']}")
-        with col2:
-            st.markdown(f"⭐ **Review Score:** {hotel['review_score']}")
-            if hotel.get("avg_distance") is not None:
-                st.markdown(f"📏 **Avg. Distance to Attractions:** {hotel['avg_distance']:.2f} km")
-
-        if hotel.get("distances_to_attractions") and st.session_state.valid_attractions:
-            with st.expander("📌 Distances to Nearby Attractions"):
-                for attr, dist in zip(st.session_state.valid_attractions, hotel["distances_to_attractions"]):
-                    st.markdown(f"- **{attr['name']}**: {dist:.2f} km")
-
-
-    # st.subheader("Tourist Attractions")
-    # for attr in st.session_state.valid_attractions:
-    #     st.markdown(f"- **{attr['name']}** — {attr['location']}")
+    app.visualize_graph()
+    st.info("Image saved!")
+    # reponse = app.run(country, city, user_description, hotel_params)
+    # st.write(response)
