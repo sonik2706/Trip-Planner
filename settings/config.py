dfdc4323509584335329621d6cb20ce89761fadb
sonek2706 <s26092@pjwstk.edu.pl>
2025-05-25 12:24:49 +0200

    Remove LLM_MODEL env variable

--- settings/config.py
+++ settings/config.py
@@ -2,8 +2,11 @@
 config.py
 
 Loads configuration settings from environment variables using Pydantic:
-- OpenAI API key
-
+- GEMINI API key
+- TAVILY API key
+- DISTANCE_MATRIX API key
+- BOOKING API key
+- BOOKING API HOST key
 """
 
 from pydantic_settings import BaseSettings
@@ -12,10 +15,9 @@ from pydantic_settings import BaseSettings
 class Config(BaseSettings):
     GEMINI_API_KEY: str
     TAVILY_API_KEY: str
-    LLM_MODEL: str
     DISTANCE_MATRIX_API_KEY: str
     BOOKING_API_KEY: str
-    BOOKING_API_HOST: str    
+    BOOKING_API_HOST: str
 
     class Config:
         env_file = ".env"
